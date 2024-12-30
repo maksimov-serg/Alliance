524c228f6002ca1d86a7a1053f98261cce3313e6
maksimov-serg <sergmaks0606@gmail.com>
2024-12-30 20:07:31 +0100

    add: slider

--- js/main.js
+++ js/main.js
@@ -35,10 +35,32 @@ mMenuToggle.addEventListener("click", (event) => {
   menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
 
-const swiper = new Swiper(".swiper", {
+const swiperSteps = new Swiper(".steps-slider", {
   speed: 400,
-  autoHeight: true,
-  slidesPerView: 5,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const swiper1 = new Swiper(".features-slider", {
+  speed: 400,
+  slidesPerView: 1,
   navigation: {
     nextEl: ".slider-button-next",
     prevEl: ".slider-button-prev",
