aa94a65097b732511f23be5caabc6ca4ca410913
maksimov-serg <sergmaks0606@gmail.com>
2025-01-08 00:23:42 +0100

    fix: modal-script

--- js/main.js
+++ js/main.js
@@ -93,3 +93,23 @@ const swiperBlog = new Swiper(".blog-slider", {
     prevEl: ".blog-button-prev",
   },
 });
+
+const modal = dokument.querySelector(".modal");
+const modalDialog = dokument.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
+  }
+});
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
+});
